e068101f25bda085c0119d512eb96abad7b8657f
ttd41 <ttda888@gmail.com>
2026-09-10 15:04:23 -0700

    Add: swim-in-rising-water - submission-4

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+
+
+    public int swimInWater(int[][] grid) {
+        int[][] paths = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[2] - b[2]));
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        visited[0][0] = true;
+        pq.add(new int[]{0, 0, grid[0][0]});
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int i = curr[0];
+            int j = curr[1];
+            if (i == m - 1 && j == n - 1) return curr[2];
+            for (int[] p: paths) {
+                int x = i + p[0];
+                int y = j + p[1];
+                if (x < 0 || y < 0 || x >= m || y >= n || visited[x][y]) continue;
+                int d = Math.max(curr[2], grid[x][y]);
+                visited[x][y] = true;
+                pq.add(new int[]{x, y, d});
+            }
+        }
+
+        return -1;
+    }
+}

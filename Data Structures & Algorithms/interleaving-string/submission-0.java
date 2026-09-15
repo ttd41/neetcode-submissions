f959a07b3337a5332a8c9e6e271e7420267a66aa
ttd41 <ttda888@gmail.com>
2026-09-15 16:54:01 -0700

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
+        if (l1 + l2 != l3) return false;
+
+        boolean[][] dp = new boolean[l1 + 1][l2 + 1];
+        dp[0][0] = true;
+
+        for (int i = 0; i <= l1; i++) {
+            for (int j = 0; j <= l2; j++) {
+                if (i > 0 && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] |= dp[i - 1][j];
+                }
+                if (j > 0 && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] |= dp[i][j - 1];
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+}

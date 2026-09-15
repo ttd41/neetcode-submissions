b9b829b048a1f963ad4590c75f6b2cb9c4cce9df
ttd41 <ttda888@gmail.com>
2026-09-15 13:47:20 -0700

    Add: coin-change - submission-11

---
+++ Data Structures & Algorithms/coin-change/submission-11.java
@@ -0,0 +1,23 @@
+class Solution {
+
+
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp, amount + 1);
+        }
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j >= coins[i]) {
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+       return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+
+}

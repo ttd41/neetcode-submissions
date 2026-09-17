d97b1b1e30eb05b4f4b1edd1d57304733d796a83
ttd41 <ttda888@gmail.com>
2026-09-16 21:22:33 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int curEnd = 0;
+        int far = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            far = Math.max(far, i + nums[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = far;
+            }
+        }
+
+        return jumps;
+    }
+}
